c5fd8107e7eb3daba4d43c84aef41b32bb083522
pengyigan_pyg <p3164181443@gmail.com>
2026-06-05 11:32:19 +0800

    fix: 理清repo层职责

--- trial-one/src/repositories/deviceRepo.js
+++ trial-one/src/repositories/deviceRepo.js
@@ -6,21 +6,34 @@ const DEVICE_CACHE_KEY = (deviceid) => `cache_device_${deviceid}`;
 const DEVICE_LIST_CACHE_KEY = (apikey) => `cache_devices_${apikey}`;
 
 async function createDevice(data) {
+    // 如果mongo出错，直接将异常向上冒，这层只做原子性的处理，出错补偿
     const newDevice = await mongoUtil.create(device, data);
     const cacheValue = {
         online: newDevice.online,
         state: newDevice.state,
         apikey: newDevice.apikey
     };
-    await redisUtil.setString(DEVICE_CACHE_KEY(newDevice.deviceid), cacheValue);
+    try {
+        await redisUtil.setString(DEVICE_CACHE_KEY(newDevice.deviceid), cacheValue);
+    } catch (err) {
+        await mongoUtil.deleteOne(device, {deviceid: newDevice.deviceid});
+        throw err;
+    }
     return newDevice;
 }
 
 async function findDeviceListByApikey(apikey) {
-    const cached = await redisUtil.getString(DEVICE_LIST_CACHE_KEY(apikey));
+    let cached = null;
+    try {
+        cached = await redisUtil.getString(DEVICE_LIST_CACHE_KEY(apikey));
+    } catch (err) {
+        // 读缓存失败不影响后续数据库查询
+    }
+
     if(cached) {
         return cached;
     }
+
     const list = await mongoUtil.findAll(device, {apikey});
     const result = {devices: {}};
     for (let ele of list) {
@@ -30,16 +43,26 @@ async function findDeviceListByApikey(apikey) {
             apikey: ele.apikey
         };
     }
-    await redisUtil.setString(DEVICE_LIST_CACHE_KEY(apikey), result);
+    try {
+        await redisUtil.setString(DEVICE_LIST_CACHE_KEY(apikey), result);
+    } catch (err) {
+        // 写缓存失败不影响后续返回
+    }
     return result;
 }
 
 async function findDeviceByDeviceid(deviceid) {
-    const cached = await redisUtil.getString(DEVICE_CACHE_KEY(deviceid));
-    console.log("【cache】", cached);
+    let cached = null;
+    try {
+        cached = await redisUtil.getString(DEVICE_CACHE_KEY(deviceid));
+    } catch (err) {
+        // 如果查缓存失败，不影响后续逻辑
+    }
+            
     if(cached) {
         return cached;
     }
+    // 查数据库，查不到往上冒
     const doc = await mongoUtil.findOne(device, {deviceid});
     if(doc) {
         const cacheValue = {
@@ -47,7 +70,11 @@ async function findDeviceByDeviceid(deviceid) {
             online: doc.online,
             state: doc.state
         };
-        await redisUtil.setString(DEVICE_CACHE_KEY(deviceid), cacheValue);
+        try {
+            await redisUtil.setString(DEVICE_CACHE_KEY(deviceid), cacheValue);
+        } catch (err){
+            // 重构缓存失败，跳过
+        }
     }
     return doc;
 }
@@ -70,8 +97,12 @@ async function updateDeviceState(deviceid, state) {
 }
 
 async function deleteDevice(deviceid) {
-    await mongoUtil.deleteOne(device, { deviceid });
-    await redisUtil.delString(DEVICE_CACHE_KEY(deviceid));
+    await mongoUtil.deleteOne(device, {deviceid});
+    try {
+        await redisUtil.delString(DEVICE_CACHE_KEY(deviceid));
+    } catch (err) {
+        // 删失败，等ttl过期
+    }
 }
 
 module.exports = {
